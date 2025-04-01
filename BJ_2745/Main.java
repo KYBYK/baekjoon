0685648cd27130ea9e4712943fbdd34593887e8b
Kim YuBin <dbqks56231106@gmail.com>
2025-04-01 22:30:06 +0900

    백준 : 일반수학 1단계 : 진법 변환 ❌⭕
    - pakage를 포함하여 백준 제출하여 컴파일 오류 발생

---
+++ BJ_2745/Main.java
@@ -0,0 +1,37 @@
+package BJ_2745;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bfr.readLine());
+
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+
+        int decimal = toDecimal (N,B);
+
+        System.out.println(decimal);
+
+        bfr.close();
+    }
+
+    public static int toDecimal(String N, int B) {
+        int decimal = 0;
+        int power = 1;
+
+        for (int i = N.length() - 1; i >= 0; i--) {
+            char ch = N.charAt(i);
+            int value = (Character.isDigit(ch)) ? ch - '0' : ch - 'A' + 10;
+            decimal += value * power;
+            power *= B;
+        }
+
+        return decimal;
+    }
+}
