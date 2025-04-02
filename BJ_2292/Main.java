cbc48fa1a568be7250cae07a810ad98e079c2887
Kim YuBin <dbqks56231106@gmail.com>
2025-04-02 20:57:16 +0900

    백준 : 일반수학 1단계 : 벌집 ⭕

---
+++ BJ_2292/Main.java
@@ -0,0 +1,25 @@
+package BJ_2292;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        int count = 1; 
+        int range = 2; 
+        
+        if (N > 1) {
+            while (range <= N) {
+                range = range + (6 * count);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        scanner.close();
+    } 
+}
