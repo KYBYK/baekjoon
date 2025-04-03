a5d6d47e4012a63ce96cb4552015de6009453863
Kim YuBin <dbqks56231106@gmail.com>
2025-04-03 22:12:09 +0900

    백준 : 일반수학 1단계 : 분수찾기 ❌⭕
    ❌ 틀린 이유
    1. a==1 b==1 가 아닌 코드 부분 a와b 증감을 반대로 입력

---
+++ BJ_1193/Main.java
@@ -0,0 +1,41 @@
+package BJ_1193;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(bfr.readLine());
+
+        int a = 1;
+        int b = 1;
+
+        boolean moveRight = true;
+
+        for(int i = 1; i < X; i++) {
+            if(moveRight) {
+                if(a == 1) {
+                    b++;
+                    moveRight = false;
+                } else {
+                    a--;
+                    b++;
+                }
+            }
+            else {
+                if(b == 1) {
+                    a++;
+                    moveRight = true;
+                } else {
+                    b--;
+                    a++;
+                }
+            }
+        }
+
+        System.out.println(a + "/" + b);
+    }
+}
