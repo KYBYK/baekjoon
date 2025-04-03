221b1c7ff79c10a08f28d6a36508a4bc8e84a66f
Kim YuBin <dbqks56231106@gmail.com>
2025-04-03 22:57:18 +0900

    백준 : 일반수학 1단계 : 달팽이는 올라가고 싶다 ❌⭕

---
+++ BJ_2869/Main.java
@@ -0,0 +1,24 @@
+package BJ_2869;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException{
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stt = new StringTokenizer(bfr.readLine());
+
+        int dailyClimb = Integer.parseInt(stt.nextToken());
+        int nightSlip = Integer.parseInt(stt.nextToken());
+        int goalHeight = Integer.parseInt(stt.nextToken());
+
+        int days = (int) Math.ceil((double)(goalHeight - nightSlip) / (dailyClimb - nightSlip));
+
+        System.out.println(days);
+
+        bfr.close();
+    }
+}
