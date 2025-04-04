67cc48dabd11827bdec2b9deeecd1bec5803cfc1
Kim YuBin <dbqks56231106@gmail.com>
2025-04-04 16:58:16 +0900

    백준 : 약수, 배수와 소수 : 배수와 약수 ⭕
    - AI 피드백
    1. 문자열 상수 정의

---
+++ BJ_5086/Main.java
@@ -0,0 +1,43 @@
+package BJ_5086;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+    
+        StringBuilder stb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer stt = new StringTokenizer(bfr.readLine());
+
+            int A = Integer.parseInt(stt.nextToken());
+            int B = Integer.parseInt(stt.nextToken());
+
+            if(A == 0 && B == 0) {
+                break;
+            }
+
+            stb.append(calculateRelation(A, B)).append("\n");
+        }
+
+        System.out.println(stb.toString());
+
+        bfr.close();
+    }
+
+    public static String calculateRelation(int A, int B) {
+        if(A % B == 0) {
+            return "factor";
+        }
+        if(B % A == 0) {
+            return "multiple";
+        }
+
+        return "neither";
+    }
+}
