2054f5f05d8596d4673d9552ef0dbe58594142fd
Kim YuBin <dbqks56231106@gmail.com>
2025-04-08 18:47:42 +0900

    백준 : 약수, 배수와 소수 : 소인수분해 ⭕
    - AI 피드백
    1. 소인수분해도 N의 제곱근까지 구하는 알고리즘 적용하면 효율적이다.
      - 코드수정 후 128ms -> 108ms로 시간 단축

---
+++ BJ_11653/Main.java
@@ -0,0 +1,34 @@
+package BJ_11653;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        if(N != 1) {
+            String result = calculatePrimeFactorsAndFormatResult(N);
+            System.out.println(result);
+        }
+    }
+
+    public static String calculatePrimeFactorsAndFormatResult(int number) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 2; i <= number; i++) {
+            while(number % i == 0) {
+                sb.append(i).append("\n");
+                number /= i;
+            }
+        }
+
+        return sb.toString();
+    }
+}
