8ec221605cf748417f05d63cc7c5031ad72f5f9e
Kim YuBin <dbqks56231106@gmail.com>
2025-04-11 17:26:57 +0900

    refactor : 사용하지 않은 class 제거

--- BJ_9063/Main.java
+++ BJ_9063/Main.java
@@ -2,10 +2,6 @@ package BJ_9063;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
