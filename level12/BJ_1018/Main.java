d43f1cc7d2e7b65cea6a72483d4cf0e29c95ba00
Kim YuBin <dbqks56231106@gmail.com>
2025-04-15 23:01:10 +0900

    백준 : 브루트 포스 : 체스판 다시 칠하기 ❓
    - 구현이 어려워 풀이 참조
    -> 다시 풀기 위해 TODO 주석 남겨놓음

---
+++ level12/BJ_1018/Main.java
@@ -0,0 +1,20 @@
+package level12.BJ_1018;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    // TODO : 다시 풀기
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+
+
+
+        bf.close();
+    }
+}
