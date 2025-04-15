969b8a4b06a9ab53746f1646072850db15b0b134
Kim YuBin <dbqks56231106@gmail.com>
2025-04-15 17:12:52 +0900

    feat : 직관성을 높이기 위해 변수명, 메서드 재작성 및 자리수 계산 로직 분리

--- level12/BJ_2231/Main.java
+++ level12/BJ_2231/Main.java
@@ -4,39 +4,43 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bf.readLine());
-        int digitSum = findMinDigitSum(N);
 
-        System.out.println(digitSum);
+        int constructor = findConstructor(N);
+
+        System.out.println(constructor);
+
+        bf.close();
     }
 
-    public static int findMinDigitSum(int N) {
-        final int NOT_FIND_DIGIT_SUM = 0;
-        int digits = countDigits(N);
-        int targetMinNum = N - (digits * 9);
+    public static int findConstructor(int N) {
+        final int NO_CONSTRUCTOR = 0;
 
-        for(int i = targetMinNum; i < N; i++) {
-            int checkNum = i;
-            int sum = 0;
+        int minPossible = N - countDigits(N)*9;
 
-            while (checkNum > 0) {
-				sum += checkNum % 10;
-				checkNum /= 10;
-            }
-
-            if(sum + i == N) {
+        for (int i = minPossible; i < N; i++) {
+            if (i + digitSum(i) == N) {
                 return i;
             }
         }
 
-        return NOT_FIND_DIGIT_SUM;
+        return NO_CONSTRUCTOR;
     }
 
     public static int countDigits(int N) {
         return Integer.toString(N).length();
+    }
+
+    public static int digitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
